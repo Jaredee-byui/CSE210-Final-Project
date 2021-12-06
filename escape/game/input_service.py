d390a0cdc9f6b6e477faaef2e6d61297dd50e794
jaredee-byui <jaredee@byui.edu>
2021-12-06 10:39:34 -0700

    moved files into assets

--- escape/game/input_service.py
+++ escape/game/input_service.py
@@ -1,5 +1,5 @@
 import sys
-import raylibpy
+import pyray
 
 class InputService:
     """Detects player input. The responsibility of the class of objects is to detect player keypresses and translate them into a point representing a direction (or velocity).
@@ -28,7 +28,7 @@ class InputService:
         Returns:
             string: The letter that was typed.
         """
-        key_int = raylibpy.get_key_pressed()
+        key_int = pyray.get_key_pressed()
         
 
         key_string = None
@@ -46,4 +46,4 @@ class InputService:
         """
         Determines if the user is trying to close the window
         """
-        return raylibpy.window_should_close()
+        return pyray.window_should_close()
--- escape/game/room.py
+++ escape/game/room.py
@@ -1,15 +1,9 @@
 class Room:
-    def __init__(self, visit_status, intro_1, unique_text, ending_1, ending_2, adjacent_rooms, actions):
-        self._has_visted_room = visit_status
+    def __init__(self, intro_1, unique_text, ending_1, ending_2):
         self._room_intro_1 = self.create_scene_string(intro_1)
         self._unique_text = self.create_scene_string(unique_text)
-        self._adjacent_rooms = adjacent_rooms
-        self._actions = actions
         self._ending_1 = self.create_scene_string(ending_1)
         self._ending_2 = self.create_scene_string(ending_2)
-
-    def get_adjacent_rooms(self):
-        return self._adjacent_rooms
 
     def get_unique_text(self):
         return self._unique_text
@@ -23,12 +17,6 @@ class Room:
     def get_ending_2(self):
         return self._ending_2
 
-    def get_visit_status(self):
-        return self._has_visted_room
-
-    def set_visit_status(self, status):
-        self._has_visted_room = status
-
     def create_scene_string(self, file_name):
         if file_name == " ":
             pass
@@ -36,6 +24,3 @@ class Room:
             with open(file_name,"rt") as work_file:
                 work_string = work_file.read()
             return work_string
-
-    def get_actions(self):
-        return self._actions
